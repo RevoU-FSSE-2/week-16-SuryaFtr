4530b6c513d2420aacdb94689fa5e44836835da4
Surya Faturohman <suryafaturohman@gmail.com>
2023-10-21 05:15:10 +0700

    add link to cors whitelist

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,7 +4,11 @@ module.exports = (app) => {
     // Use Cors for Global
     app.use(cors(
         {
-            origin: ['https://tame-teal-dog-cap.cyclic.app', 'http://localhost:5173']
+            origin: [
+                'https://effortless-seahorse-90ef87.netlify.app',
+                'https://week-17-suryafaturohman.web.app/',
+                'http://localhost:5173'
+            ]
         }
     ));
 }
